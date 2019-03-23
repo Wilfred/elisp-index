ca1ffadb01b1a61584c6788c151fb162978b2b14
Wilfred Hughes <me@wilfred.me.uk>
2019-03-23 15:11:17 +0000

    Read function names

--- elisp-index.el
+++ elisp-index.el
@@ -26,26 +26,57 @@
 
 (defun elisp-index--symbols (buf)
   (let ((read-with-symbol-positions t)
-        (syms))
+        syms)
     (with-current-buffer buf
       (goto-char (point-min))
       (condition-case err
           (while t
             (read buf)
             (setq syms
-                  (append syms read-symbol-positions-list))
-            )
+                  (append syms read-symbol-positions-list)))
         (error
          (if (equal (car err) 'end-of-file)
              syms
            ;; Some unexpected error, propagate.
            (error "Unexpected error whilst reading %s position %s: %s"
-                  path (point) err)))))))
+                  (buffer-file-name) (point) err)))))))
+
+(defun elisp-index--function-def-p (form)
+  ;; TODO: return a list of all the functions defined in this form.
+  (let* ((expanded (macroexpand-all form))
+         (head-sym (car-safe expanded))
+         (fun-name (car-safe (cdr-safe expanded)))
+         (body (car-safe (cdr-safe (cdr-safe expanded)))))
+    (when
+        (and (eq head-sym 'defalias)
+             (eq (car-safe body) 'function))
+      (nth 1 fun-name))))
+
+(defun elisp-index--functions (buf)
+  (let ((read-with-symbol-positions t)
+        funs)
+    (with-current-buffer buf
+      (goto-char (point-min))
+      (condition-case err
+          (while t
+            (let* ((form (read buf))
+                   (start-pos (scan-sexps (point) -1))
+                   (fun-sym (elisp-index--function-def-p form)))
+              (when fun-sym
+                (push (cons fun-sym start-pos) funs))))
+        (error
+         (if (equal (car err) 'end-of-file)
+             (nreverse funs)
+           ;; Some unexpected error, propagate.
+           (error "Unexpected error whilst reading %s position %s: %s"
+                  (buffer-file-name) (point) err)))))))
 
 (defun elisp-index (path)
   (let ((buf (find-file-noselect path))
         )
-    (elisp-index--symbols buf)
+    (list
+     :functions (elisp-index--functions buf)
+     :symbols (elisp-index--symbols buf))
     ))
 
 (provide 'elisp-index)
