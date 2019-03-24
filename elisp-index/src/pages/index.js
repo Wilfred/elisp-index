64950e7f840b7f654238d8f057fcd9b5437d754d
Wilfred Hughes <me@wilfred.me.uk>
2019-03-24 23:48:56 +0000

    Render symbol names on gatsby home page

--- elisp-index/src/pages/index.js
+++ elisp-index/src/pages/index.js
@@ -1,21 +1,32 @@
 import React from "react";
-import { Link } from "gatsby";
+import { graphql } from "gatsby";
 
 import Layout from "../components/layout";
-import Image from "../components/image";
 import SEO from "../components/seo";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people!</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-);
+const IndexPage = ({ data }) => {
+  const functions = data.dataJson.functions.map(fn => (
+    <li key={fn.name}>
+      {fn.name}: {fn.position}
+    </li>
+  ));
+  return (
+    <Layout>
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      <ol>{functions}</ol>
+    </Layout>
+  );
+};
 
 export default IndexPage;
+
+export const query = graphql`
+  {
+    dataJson {
+      functions {
+        position
+        name
+      }
+    }
+  }
+`;
