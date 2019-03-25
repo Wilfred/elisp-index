0447595513ff4240ae0a115796287db0a54a6a66
Wilfred Hughes <me@wilfred.me.uk>
2019-03-25 23:35:08 +0000

    Prefer Bulma styling

--- elisp-index/src/components/header.js
+++ elisp-index/src/components/header.js
@@ -9,14 +9,8 @@ const Header = ({ siteTitle }) => (
       marginBottom: `1.45rem`
     }}
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+    <div className="container">
+      <h1>
         <Link
           to="/"
           style={{
