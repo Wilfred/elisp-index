53a60fc197c283032ecfed6dd11eaf27ae621ea3
Wilfred Hughes <me@wilfred.me.uk>
2019-03-27 09:56:31 +0000

    Fix site config placeholder values

--- elisp-index/gatsby-config.js
+++ elisp-index/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`
+    title: `Elisp Index`,
+    description: `A cross-referenced code listing of the Emacs Lisp ecosystem`,
+    author: `Wilfred Hughes`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
