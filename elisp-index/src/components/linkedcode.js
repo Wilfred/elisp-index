6aa85542b5c1366aefc4eebe925d310329929481
Wilfred Hughes <me@wilfred.me.uk>
2019-04-02 10:07:42 +0100

    Split source code into separate lines wrapped in <code>

--- elisp-index/src/components/linkedcode.js
+++ elisp-index/src/components/linkedcode.js
@@ -18,24 +18,61 @@ const RefLink = ({ call, fnNameToFile }) => {
   );
 };
 
+function butLast(arr) {
+  return arr.slice(0, arr.length - 1);
+}
+
+function last(arr) {
+  return arr[arr.length - 1];
+}
+
 const LinkedCode = ({ source, calls, fnNameToFile }) => {
-  let parts = [];
+  let lines = [];
+  let currentLine = [];
+
+  function appendText(text) {
+    let textLines = text.split("\n");
+
+    if (textLines.length == 1) {
+      // Text is not a whole line, add it to current line.
+      currentLine.push(text);
+    } else {
+      // Text is several lines, add them to lines directly.
+      lines.push(currentLine);
+      currentLine = [];
+      butLast(textLines).forEach(prefixLine => {
+        lines.push(<code>{prefixLine + "\n"}</code>);
+      });
+      currentLine.push(last(textLines));
+    }
+  }
+
   let i = 0;
   calls.forEach(call => {
     if (call.start > i) {
-      parts.push(source.substring(i, call.start));
+      let prefix = source.substring(i, call.start);
+      appendText(prefix);
     }
 
-    parts.push(<RefLink call={call} fnNameToFile={fnNameToFile} />);
+    currentLine.push(<RefLink call={call} fnNameToFile={fnNameToFile} />);
     i = call.end;
   });
   if (i < source.length) {
-    parts.push(source.substring(i));
+    appendText(source.substring(i));
+  }
+
+  if (currentLine.length > 0) {
+    lines.push(
+      <code>
+        {currentLine}
+        {"\n"}
+      </code>
+    );
   }
 
   return (
     <div>
-      <pre>{parts}</pre>
+      <pre>{lines}</pre>
     </div>
   );
 };
